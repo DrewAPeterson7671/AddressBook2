6e8ae75b77b95642707da85c35a0889ef5dce4aa
Drew Peterson <drew.a.peterson@hotmail.com>
2020-12-14 13:56:38 -0800

    Add unique IDs

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,38 @@
-$(document).ready(function() {
+function AddressBook() {
+  this.contacts = {};
+  this.currentId = 0;
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+}
+
+function Contact(firstName, lastName, phoneNumber) {
+ this.firsName = firstName;
+ this.lastName = lastName;
+ this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastname;
+}
 
 
+// let addressBook = new AddressBook();
+// let contact = new Contact("Ada", "Lovelace", "503-555-0100");
+// let contact2 = new Contact("Grace", "Hopper", "503-555-0199");
+// addressBook.addContact(contact);
+// addressBook.addContact(contact2);
+// addressBook;
 
 
-  
-})
+//$(document).ready(function() {
+
+//})
 
